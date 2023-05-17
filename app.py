7ac008fe21dc2f1b68144426f0714f125eec4ab8
zidanefrl <zidanefrl48@gmail.com>
2023-05-17 08:49:44 +0700

    Initialize

--- app.py
+++ app.py
@@ -9,23 +9,23 @@ from IPython import get_ipython
 from PIL import Image
 
 # load the encoder and model object
-model = joblib.load(rta_model_deploy3.joblib)
-encoder = joblib.load(ordinal_encoder2.joblib)
+model = joblib.load("rta_model_deploy3.joblib")
+encoder = joblib.load("ordinal_encoder2.joblib")
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
-# 1 serious injury, 2 Slight injury, 0 Fatal Injury
+# 1: serious injury, 2: Slight injury, 0: Fatal Injury
 
-st.set_page_config(page_title=Accident Severity Prediction App,
-        page_icon=🚧, layout=wide)
+st.set_page_config(page_title="Accident Severity Prediction App",
+        page_icon="🚧", layout="wide")
 
 #creating option list for dropdown menu
-options_day = ['Sunday', Monday, Tuesday, Wednesday, Thursday, Friday, Saturday]
+options_day = ['Sunday', "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 options_age = ['18-30', '31-50', 'Over 51', 'Unknown', 'Under 18']
 
-# number of vehicle involved range of 1 to 7
-# number of casualties range of 1 to 8
-# hour of the day range of 0 to 23
+# number of vehicle involved: range of 1 to 7
+# number of casualties: range of 1 to 8
+# hour of the day: range of 0 to 23
 
 options_types_collision = ['Vehicle with vehicle collision','Collision with roadside objects',
               'Collision with pedestrians','Rollover','Collision with animals',
